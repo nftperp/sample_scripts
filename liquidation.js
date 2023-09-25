632166b7e3fc069928c69ba43d30d26897b4da82
warproxxx <n00bdan13@gmail.com>
2023-09-25 14:23:53 -0700

    Update the liquidation logic

--- liquidation.js
+++ liquidation.js
@@ -3,6 +3,7 @@ const axios = require("axios").default;
 const { fetchPositions } = require("./fetchPositions");
 const { getTradersByAmm, updateTrader, deleteTrader } = require('./stateManager');
 const ERC20_ABI = require("./abi/ERC20.json");
+const AMM_ABI = require("./abi/AMM.json");
 
 require("dotenv").config();
 
@@ -41,9 +42,25 @@ async function main(){
         //loop thru now to look at liquidation
         for (const amm in tradersByAmm) {
             for (const trader in tradersByAmm[amm]) {
+                
+                
+
+
                 if (await contract.isLiquidatable(amm, trader)){
+                    let amm_contract = new ethers.Contract(amm, AMM_ABI['abi'], signer);
+                    let position = await contract.getPosition(amm, trader);
+
+                    let liquidationPrice = await amm_contract.getLiquidationPrice();
+                    let positionNotional = position.size.mul(liquidationPrice).div(ethers.utils.parseEther('1'))
+
+                    console.log("------------------")        
+                    console.log(ethers.utils.formatEther(position.size))
+                    console.log(ethers.utils.formatEther(liquidationPrice.toString()))
+                    console.log(ethers.utils.formatEther(positionNotional.toString()))
+                    console.log("------------------")
+
                     try {
-                        await contract.liquidate(amm, trader, String(1 * 10**18)); //the last part is the margin. 1 ether means a margin of 1X
+                        await contract.liquidate(amm, trader, positionNotional); //the last element reperesents the margin you want the new position to take. This new position will have a margin of ~1X.
                         deleteTrader(amm, trader);
                         console.log(`Successfully liquidated ${trader} in ${amm}`);
                     } catch (error) {
