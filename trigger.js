d73042199b141f383229fc8383ea3e9dc1d5c240
warproxxx <n00bdan13@gmail.com>
2023-10-05 09:49:23 -0700

    Even simpler

--- trigger.js
+++ trigger.js
@@ -26,60 +26,13 @@ async function perform_trigger(contract, amms){
     isRunning = true;
 
     try{
-        
-
         let triggerOrders = await axios.get(`https://api.nftperp.xyz/orders/trigger`)
 
         for (const triggerOrder of triggerOrders.data.data) {
             let amm = amms[triggerOrder.amm]
-            let pos = await contract.getPosition(amm, triggerOrder.trader)
-            let direction;
-
-            if (pos.size > 0){
-                direction = 'long'
-            } else {
-                direction = 'short'
-            }
-
-            if (parseFloat(pos.size) > 0){
-                direction = 'long'
-            } else {
-                direction = 'short'
-            }
-            let liquidatable = await contract.isLiquidatable(amm, triggerOrder.trader)
-
-            if (parseFloat(pos.size) != 0 & liquidatable == false){
-                let ammContract = new ethers.Contract(amm, AMM_ABI['abi'], signer);
-                let markPrice =  ethers.utils.formatEther(await ammContract.getMarkPrice())
-
-                let valid;
-
-                if (direction == 'long'){
-                    if (triggerOrder.takeProfit) {
-                        valid = parseFloat(markPrice) >= parseFloat(triggerOrder.trigger);
-                    } else {
-                        valid = parseFloat(markPrice) <= parseFloat(triggerOrder.trigger);
-                    }
-                } else {
-                    if (triggerOrder.takeProfit) {
-                        valid = parseFloat(markPrice) <= parseFloat(triggerOrder.trigger);
-                    } else {
-                        valid = parseFloat(markPrice) >= parseFloat(triggerOrder.trigger);
-                    }
-                }
-
-                console.log("takeProfit: " + triggerOrder.takeProfit + 
-                            ", markPrice: " + parseFloat(markPrice) + 
-                            ", trigger: " + parseFloat(triggerOrder.trigger) + 
-                            ", direction: " + direction + 
-                            ", size: " + pos.size + 
-                            ", valid:" + valid);
-
-                if (valid){
-                    console.log(`Trigger Order with ID ${triggerOrder.id} has been triggered at mark price ${markPrice} on the ${triggerOrder.amm} AMM`);
-                    await contract.closePositionKeeper(amm, triggerOrder.id)
-                }
-
+            if (await contract.isTriggerOrderValid(amm, triggerOrder.id)){
+                await contract.closePositionKeeper(amm, triggerOrder.id)
+                console.log(`Trigger Order with ID ${triggerOrder.id} has been triggered at on the ${triggerOrder.amm} AMM`);
             }
 
         }
